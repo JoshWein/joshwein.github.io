fd21910c54a993de4fb95e6973204fb60a576394
Josh Wein <jdog90000@yahoo.com>
2015-09-04 02:42:31 -0400

    Massive upgrade, added colors and better spacing. Implemented smooth JQuery scrolling. Added a lot of css styling.

--- js/script.js
+++ js/script.js
@@ -7,13 +7,30 @@ $("#pb1").animate({
 }, 500);
 
 $("#pb2").animate({
-    width: "70%"
+    width: "75%"
 }, 500);
 
 $("#pb3").animate({
-    width: "30%"
+    width: "45%"
 }, 500);
 
 $("#pb4").animate({
+    width: "30%"
+}, 500);
+
+$("#pb5").animate({
     width: "20%"
 }, 500);
+
+$('a[href*=#]:not([href=#])').click(function() {
+if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  var target = $(this.hash);
+  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+  if (target.length) {
+    $('html,body').animate({
+      scrollTop: target.offset().top - $('nav').height()
+    }, 1000);
+    return false;
+  }
+}
+});
