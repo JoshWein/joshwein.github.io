554a613bfaa19e5480f51d337a56774f7ee70d8c
Josh Wein <joshua.wein@stonybrook.edu>
2015-09-03 20:42:31 -0400

    Better formatting, single collumn style, animated progress bars.

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$('.progress-bar').css({    
+    'background-color': 'rgb(114, 186, 247)'
+});
+
+$("#pb1").animate({
+    width: "100%"
+}, 500);
+
+$("#pb2").animate({
+    width: "70%"
+}, 500);
+
+$("#pb3").animate({
+    width: "30%"
+}, 500);
+
+$("#pb4").animate({
+    width: "20%"
+}, 500);
