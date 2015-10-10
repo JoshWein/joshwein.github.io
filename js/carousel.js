7a5cbf4142ab4256b1247177d8882bf3cab3c6bd
Josh Wein <jdog90000@yahoo.com>
2015-10-10 00:01:17 -0400

    Fixed carousel buttons, added javascript, enabled cycling.

---
+++ js/carousel.js
@@ -0,0 +1,15 @@
+// Activate Carousel
+$("#myCarousel").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+    $("#myCarousel").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+});
+$(".right").click(function(){
+    $("#myCarousel").carousel("next");
+});
